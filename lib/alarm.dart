104f69b8b8a5cbce6510e3c065fba876a7d2da1c
준영 김 <wkrwjafl15@gmail.com>
2022-01-12 22:16:06 +0900

    로그인 페이지 추가, 카테고리/mbti선택 페이지 listview로 변경

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -43,7 +43,7 @@ class _alarmTabState extends State<alarmTab> {
             automaticallyImplyLeading: true,
             leading: IconButton(icon: const Icon(Icons.arrow_back),
               onPressed: () {  },),
-            ),
+            
             title: const Text('알림'),
             actions: [
               IconButton(
